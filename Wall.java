0893833142961fcd5288aa9c8ee5cc9705e5544f
Jayden Remillard <ja.d.remillard@baypath.net>
2023-06-01 11:00:15 -0400

    Wonder Wall

--- Wall.java
+++ Wall.java
@@ -1,11 +1,14 @@
 public class Wall {
     private int wallX;
     private int wallY;
-    public Wall(int x, int y) {
+    private String direction;
+    public Wall(int x, int y, String facing) {
         wallX = x;
         wallY = y;
+        direction = facing;
     }
     public int returnX() { return wallX; }
     public int returnY() { return wallY; }
+    public String returnDirection() { return direction; }
     public String toString() { return "wall"; }
 }
