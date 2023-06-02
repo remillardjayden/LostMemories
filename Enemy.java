a60714dfabdebd78463cd49f146c0234c2d53cf9
Jayden Remillard <ja.d.remillard@baypath.net>
2023-06-02 12:56:31 -0400

    Booberry

--- Enemy.java
+++ Enemy.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 public class Enemy extends Character {
+    private String description;
     public ArrayList<Enemy> allEnemies;
-    public Enemy(String name, int totalHP, String dir) {
+    public Enemy(String name, int totalHP, String dir, String desc) {
         super(name, totalHP, dir);
         if(allEnemies == null) { allEnemies = new ArrayList<Enemy>(); }
         allEnemies.add(this);
+        description = desc;
     }
+    public String getDesc() { return description; }
 }
--- MyGame.java
+++ MyGame.java
@@ -105,7 +105,7 @@ public class MyGame {
         new Item("Steve", "OUH");
         new Item("Stick", "It's sticky");
         // Enemies
-        new Enemy("Bat", 50, "Left");
+        new Enemy("Bat", 50, "Left", "He's Booberry");
         for(int r = 0; r < 25; r++) {
             for(int c = 0; c < 25; c++) {
                 if(r == user.getY() && c == user.getX()) {
