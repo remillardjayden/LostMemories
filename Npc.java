12fc7bd3537b1c33534771f9129def00ffa6fd86
Jayden Remillard <ja.d.remillard@baypath.net>
2023-06-15 08:34:56 -0400

    Minor fix

--- Npc.java
+++ Npc.java
@@ -1,14 +1,18 @@
+import java.util.ArrayList;
 public class Npc {
     private String name;
     private String[] voicelines;
     private String[] options;
     private int x, y;
+    public ArrayList<Npc> allNpcs;
     public Npc(String name, String[] voices, String[] choices, int x, int y) {
         this.name = name;
         voicelines = voices;
         options = choices;
         this.x = x;
         this.y = y;
+        if(allNpcs == null) { allNpcs = new ArrayList<Npc>(); }
+        allNpcs.add(this);
     }
     public String getCoords() { return "(" + x + ", " + y + ")"; }
     public int getX() { return x; }
